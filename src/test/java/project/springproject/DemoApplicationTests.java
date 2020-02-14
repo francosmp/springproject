db87abe740fe371ae3d2bf2bd02abdbcb64eca19
iFiL <stha-182@hotmail.com>
2020-02-13 23:12:04 -0500

    2020/02/13 23:12

---
+++ src/test/java/project/springproject/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package project.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
